3b8100a5e251c61cc38dc3733549bef0a2253cff
Mustafa Shazlie <mustafa.shazlie@gmail.com>
2025-03-19 14:02:06 +0300

    added days counter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const clock = document.getElementById("clock");
 const searchInput = document.getElementById("search");
+const daysCounter = document.getElementById("daysCounter");
 
 window.setInterval(() => {
 	let time = new Date();
@@ -7,12 +8,27 @@ window.setInterval(() => {
 		time.getHours() + ":" + String(time.getMinutes()).padStart(2, "0");
 }, 100);
 
+function initDate() {
+	let todayDate = new Date();
+	let examDate = new Date("2025-06-21");
+	let differenceInTime = examDate.getTime() - todayDate.getTime();
+	daysCounter.innerText = `${Math.floor(
+		differenceInTime / (1000 * 3600 * 24)
+	)} Days`;
+}
+
 function search() {
 	window.location = "https://www.google.com/search?q=" + searchInput.value;
 }
+
+initDate();
 
 searchInput.addEventListener("keypress", (e) => {
 	if (e.key == "Enter") {
 		search();
 	}
 });
+
+window.setInterval(() => {
+	initDate();
+}, 1000 * 60);
